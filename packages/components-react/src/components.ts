fac95032e9d3759d48c5dec51d9e3678146d9eba
Drago Vrban <drago.vrban@jsguru.io>
2022-05-25 18:05:50 +0200

    Ovde

--- packages/components-react/src/components.ts
+++ packages/components-react/src/components.ts
@@ -3,9 +3,9 @@
 /* auto-generated react proxies */
 import { createReactComponent } from './react-component-lib';
 
-import type { JSX } from '@jsguru/components';
+import type { JSX } from '@jsgurucompany/components';
 
-import { defineCustomElements } from '@jsguru/components/loader';
+import { defineCustomElements } from '@jsgurucompany/components/loader';
 
 defineCustomElements();
 export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
